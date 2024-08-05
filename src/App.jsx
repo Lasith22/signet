cda206f9a236694b661beccedb0b1500263889a2
lasith herath <lasithherath00@gmail.com>
2024-08-05 18:45:34 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
-      <h1 className="font-bold text-3xl">Signet</h1>
+      <HomePage />
     </>
   );
 }
