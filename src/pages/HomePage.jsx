0bc99847a9b6a9095ee04a4e89ecabbeda21c1a7
lasith herath <lasithherath00@gmail.com>
2024-08-07 15:35:53 +0530

    Product section finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import HeroSection from '../components/layout/HeroSection';
+import ProductCollection from '../components/sections/ProductCollection';
 
 const HomePage = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <ProductCollection />
     </>
   );
 };
