568f423b6bba60c977a217a14bb4d80ac8e9527c
lasith herath <lasithherath00@gmail.com>
2024-09-16 06:51:36 +0530

    Change footer

--- src/components/layout/HeroSection.jsx
+++ src/components/layout/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div className="bg-[#D9D9D9]">
       <h1 className="font-secondMainTextStyle font-bold text-[#000000] text-[96px] pt-60 pl-20 pb-20">
-        HIGHEST QUALITY <br /> COLLECTION
+        HIGHEST QUALITY <br /> COLLECTIONS
       </h1>
     </div>
   );
